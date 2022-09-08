6cd4c50ebeed6b8d39e75795bd3da7f0b77573d9
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-09-08 16:03:56 +0300

    Add project files.

---
+++ WinAPI/Source.cpp
@@ -0,0 +1,44 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//MessageBox(NULL, "Привет! Это окно сообщения", "Заголовок окна", MB_YESNO | MB_ICONINFORMATION | MB_HELP | MB_DEFBUTTON2 | MB_SYSTEMMODAL);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG://создаем элемент окна
+		break;
+	case WM_COMMAND://обрабатываем сообщение нажатия на кнопки
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			
+			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+			break;
+		case IDOK: MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL: 
+			EndDialog(hwnd, 0);
+			return FALSE;
+		}
+		break;
+	case WM_CLOSE://закрытие окна
+		EndDialog(hwnd, 0);
+		return FALSE;
+	}
+	return FALSE;
+}
